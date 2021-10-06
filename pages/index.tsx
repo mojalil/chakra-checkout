2d4c76907c0b6d276f265fbb6d373195af2e3b88
Mo <mo@metapair.com>
2021-10-06 17:48:31 +0800

    refactored sections into seperate components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,13 @@
-const IndexPage = () => <h1>Hello next.js</h1>
+import { Container, Flex, VStack } from "@chakra-ui/react";
+import Cart from "../src/sections/cart";
+import Details from "../src/sections/details";
+const IndexPage = () => (
+  <Container maxWidth="container.xl" padding={0}>
+    <Flex h="100vh" py={20}>
+      <Details></Details>
+      <Cart></Cart>
+    </Flex>
+  </Container>
+);
 
 export default IndexPage;
---
+++ src/sections/details.tsx
@@ -0,0 +1,15 @@
+import { VStack } from "@chakra-ui/layout";
+
+const Details = () => {
+  return (
+    <VStack
+      w="full"
+      h="full"
+      p={10}
+      spacing={10}
+      alignItems="flex-start"
+    ></VStack>
+  );
+};
+
+export default Details;
