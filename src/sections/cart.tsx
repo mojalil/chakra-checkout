534176a2ee2a93c7fb08295c5d62f525c5b21ed9
Mo <mo@metapair.com>
2021-10-06 23:46:52 +0800

    completed inital checkout page

--- src/sections/cart.tsx
+++ src/sections/cart.tsx
@@ -1,4 +1,12 @@
-import { VStack } from "@chakra-ui/layout";
+import {
+  Heading,
+  VStack,
+  Text,
+  HStack,
+  AspectRatio,
+  Stack,
+} from "@chakra-ui/layout";
+import { Image } from "@chakra-ui/react";
 
 const Cart = () => {
   return (
@@ -9,7 +17,50 @@ const Cart = () => {
       spacing={10}
       alignItems="flex-start"
       bg="gray.50"
-    ></VStack>
+    >
+      <VStack spacing={3} alignItems="flex-start">
+        <Heading size="2xl">Your Cart</Heading>
+        <Text>Your items are below</Text>
+      </VStack>
+      <HStack spacing={6} alignItems="center" w="full">
+        <AspectRatio ratio={1} w={24}>
+          <Image src="/images/skateboard.jpg" alt="Skateboard"></Image>
+        </AspectRatio>
+        <Stack
+          spacing={0}
+          w="full"
+          direction="row"
+          justifyContent="space-between"
+          alignItems="center"
+        >
+          <VStack w="full" spacing={0} alignItems="flex-start">
+            <Heading size="md">Penny Board</Heading>
+            <Text color="gray.600">PNYCOMP234597</Text>
+          </VStack>
+          <Heading size="sm" textAlign="end">
+            $119.00
+          </Heading>
+        </Stack>
+      </HStack>
+      <VStack spacing={4} alignItems="stretch" w="full">
+        <HStack justifyContent="space-between">
+          <Text color="gray.600">Subtotal</Text>
+          <Heading size="sm">$119.00</Heading>
+        </HStack>
+        <HStack justifyContent="space-between">
+          <Text color="gray.600">Shipping</Text>
+          <Heading size="sm">$19.99</Heading>
+        </HStack>
+        <HStack justifyContent="space-between">
+          <Text color="gray.600">Taxes (Estimated)</Text>
+          <Heading size="sm">$23.80</Heading>
+        </HStack>
+        <HStack justifyContent="space-between">
+          <Text color="gray.600">Total</Text>
+          <Heading size="lg">$162.79</Heading>
+        </HStack>
+      </VStack>
+    </VStack>
   );
 };
 
