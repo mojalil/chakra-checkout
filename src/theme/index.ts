96800021a34e44bb580c0195653701147c86d421
Mo <mo@metapair.com>
2021-10-07 00:41:33 +0800

    override styles for components

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,4 +1,23 @@
 import { extendTheme , theme as base, withDefaultColorScheme, withDefaultVariant} from "@chakra-ui/react";
+const inputSelectStyles =  {
+    variants: {
+        filled: {
+            field: {
+                _focus: {
+                    borderColor: 'brand.500'
+                }
+            }
+        }
+    },
+    sizes: {
+        md: {
+            field: {
+                borderRadius: 'none'
+            }
+        }
+
+    }
+}
 const theme = extendTheme({
     colors: {
         brand: {
@@ -19,6 +38,21 @@ const theme = extendTheme({
         heading: `Montserrat, ${base.fonts?.heading}`,
         body:  `Inter, ${base.fonts?.body}`,
     },
+    components: {
+        Input: {...inputSelectStyles},
+        Select: {...inputSelectStyles},
+        Checkbox: {
+            baseStyle: {
+                control: {
+                    borderRadius: 'none',
+                    _focus: {
+                        ring: 2, 
+                        ringColor: 'brand.500'
+                    }
+                }
+            }
+        }
+    }
 }, withDefaultColorScheme({
     colorScheme: 'brand',
     components: ['Checkbox']
