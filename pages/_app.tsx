86e2287beda4cd20b0294d1f8992cee546d25767
Mo <mo@metapair.com>
2021-10-06 17:26:22 +0800

    inital commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,10 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { AppProps } from 'next/app';
+import { ChakraProvider } from '@chakra-ui/react';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const App = ({ Component, pageProps } : AppProps ) => {
+    return (<ChakraProvider>
+        <Component {...pageProps}></Component>
+    </ChakraProvider>)
 }
-export default MyApp
+
+export default App;
